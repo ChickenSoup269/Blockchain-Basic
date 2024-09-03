d3000b1eba7248643d15046d4462d52baf831024
Zero_031 <95624468+ChickenSoup269@users.noreply.github.com>
2024-09-03 09:45:18 +0700

    V2 Blockchain [add Block]

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hash = require("crypto-js/sha256")
 
+// TODO: làm một cái blockchain lưu trữ dữ liệu
 class Block {
   constructor(prevHash, data) {
     this.prevHash = prevHash
@@ -15,8 +16,34 @@ class Block {
   }
 }
 
-const block = new Block("", {
-  name: "Thiendeptrai",
-})
+// const block = new Block("", {
+//   name: "Thiendeptrai",
+// })
 
-console.log(block)
+class Blockchain {
+  constructor() {
+    const genesisBlock = new Block("0000", {
+      isGenesis: true,
+    })
+    this.chain = [genesisBlock]
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1]
+  }
+
+  addBlock(data) {
+    const lastBlock = this.getLastBlock()
+    const newBlock = new Block(lastBlock.hash, data)
+
+    this.chain.push(newBlock)
+  }
+}
+
+const thienChain = new Blockchain()
+console.log(thienChain)
+
+thienChain.addBlock({ from: "thien", to: "bank", amount: "1000000" })
+thienChain.addBlock({ from: "thien", to: "steam", amount: "500000" })
+
+console.log(thienChain.chain)
